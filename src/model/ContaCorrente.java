e6914231c86ea260c245373b861ce041bf8fd46b
Tiago Tardelli <tiagob.tardelli@gmail.com>
2025-09-25 04:51:48 -0300

    Finalizando Projeto

--- src/model/ContaCorrente.java
+++ src/model/ContaCorrente.java
@@ -1,4 +1,14 @@
 package model;
 
 public class ContaCorrente extends Conta {
+
+    public ContaCorrente(Cliente cliente){
+        super(cliente);
+    }
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("=== Extrato da conta corrente ===");
+        super.imprimirInformacoes();
+    }
 }
